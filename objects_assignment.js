c3aa22a4c5f7e2e00e8bdda5f67469c768c5b792
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-21 16:07:17 +0300

    creating pseudo code

---
+++ objects_assignment.js
@@ -0,0 +1,12 @@
+//Pseudo code
+
+/*
+___________________________________________________________________________________________________
+
+Create a constructor call it Contact
+Give it attributes=> fist name, second name, phone number, email and  adress
+Assign the above attributes to values which will be the properties in the new object
+Save the address as an array contains street, city, country.
+Create a method that returns the full Address of the user in form of street + ", " + city + " " + state
+____________________________________________________________________________________________________
+*/
