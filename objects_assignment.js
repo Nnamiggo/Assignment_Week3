1ab45646ded7e4d5149267df1eb918c80e22adc1
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-21 16:19:28 +0300

    defining the constructor

--- objects_assignment.js
+++ objects_assignment.js
@@ -10,3 +10,10 @@ Save the address as an array contains street, city, country.
 Create a method that returns the full Address of the user in form of street + ", " + city + " " + state
 ____________________________________________________________________________________________________
 */
+function Contact(first_name, second_name, phone_number, address)
+	{
+		this.first_name = first_name;
+		this.second_name = second_name;
+		this.phone_number = phone_number;
+		this.address = address;
+	}
