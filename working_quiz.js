7b563002ed93ee61518d9b0372786d1c18fe28e6
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-26 04:29:41 +0300

    trial assignment

--- working_quiz.js
+++ working_quiz.js
@@ -16,5 +16,15 @@ flower["height"]=30;
 flower.pollinators =["bees", "butterflies", "birds"];
 
 //Add one more creature to your list: humans
-pollinators.push('humans');
-console.log(pollinators[pollinators.length-1]);
+flower.pollinators.push('humans');
+//console.log(flower.pollinators[flower.pollinators.length-1]);
+
+//Write a method that allows the flower to grow. After the method is run, the height value should be increased.
+flower.grow = function(){
+	//add property rainfall and temperature
+	flower.rainfall = 30;
+	flower.temperature = 20;
+	if(flower.rainfall>=30 && flower.temperature>=20){let new_height = flower.height +2; return new_height;}
+
+};
+console.log(flower.grow());
