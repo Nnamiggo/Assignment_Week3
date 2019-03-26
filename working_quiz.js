2fd7708a43870b4f2fa384e4e03ce7f49b8b2bce
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-26 04:01:36 +0300

    trial assignment

--- working_quiz.js
+++ working_quiz.js
@@ -4,11 +4,17 @@ var flower = {
 	color : "Yellow",
 	height: 30
 };
+
 //Change the color of your flower using dot notation.
 flower.color = "Yellow green";
+
 //Change the height of your flower using bracket notation.
 flower["height"]=30;
+
 //Add a property that indicates what kind of creatures
 // help the flower with pollination. This includes bees, butterflies and birds.
 flower.pollinators =["bees", "butterflies", "birds"];
-console.log(flower.pollinators);
+
+//Add one more creature to your list: humans
+pollinators.push('humans');
+console.log(pollinators[pollinators.length-1]);
