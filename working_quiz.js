44a6ee2dc7bed418ce6d4e46d473ac017f526057
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-26 03:55:10 +0300

    trial assignment

--- working_quiz.js
+++ working_quiz.js
@@ -4,3 +4,8 @@ var flower = {
 	color : "Yellow",
 	height: 30
 };
+//Change the color of your flower using dot notation.
+flower.color = "Yellow green";
+//Change the height of your flower using bracket notation.
+flower["height"]=30;
+console.log(flower.height);
