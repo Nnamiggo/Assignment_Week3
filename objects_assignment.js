d601668008555c7c93df17afdcb90afbf532013d
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-27 16:30:33 +0300

    editing code

--- objects_assignment.js
+++ objects_assignment.js
@@ -51,48 +51,52 @@ function Contact(first_name, second_name, phone_number, email, street, city, cou
 			
 			};
 
-var scosh = new Contact("scovia", "Nnamiggo", "0703268406", "wakiso", "kampala", "uganda");
-var arthur = new Contact("Arthur", "King", "0703268406", "wakiso", "kampala", "uganda");
+var scosh = new Contact("scovia", "Nnamiggo", "0703268406", "2305senk@gmail.com", "wakiso", "kampala", "uganda");
+var arthur = new Contact("Arthur", "King", "0703268406", "2305senk@gmail.com", "wakiso", "kampala", "uganda");
 
 /* implementing delete function */
 
-var deleting_you; //This holds what I want to delete either input or a constant
+var deleting_you =(my_object) => { //pass in the object you want to delete
 
 for(let i=0; i<my_address_book.length; i++)
 	{
-	if(my_address_book[i]=== deleting_you)
+	if(my_address_book[i]=== my_object)
 		{ 
 			my_address_book.splice(i,1); 
 
-			console.log(deleting_you + "has been deleted");
+			console.log(my_object + "has been deleted");
 			
 		} 
 	}
+}
 
 /* implementing edit code */
 
-
-var editing_you; //This holds the object whose properties I want to edit either input or a constant
+//function takes in name of the object and some values you want to change in that object
+var editing_you=(name_of_object, new_value1, new_value2, new_value3, new_valueetc) => { 
 
 var gona_edit_you=false; //This turns to true if the instance is in the array that holds instance
 
 for(let i=0; i<my_address_book.length; i++)
 	{
-	if(my_address_book[i]=== editing_you)
+	if(my_address_book[i]=== name_of_object)
 		{ 
-			var gona_edit_you = true;
+			gona_edit_you = true;
 		} 
-	}
-
+	
 	if(gona_edit_you==true) //if object was found then.....
-	{ 
-		//change some values
-		var object_index_array = Object.keys(editing_you);
-			object_index_array[0] = "new value"; //editing value at first index
-			object_index_array[1] = "new value"; //editing value at second index
-			object_index_array[2] = "new value"; //editing value at third index
-			object_index_array[3] = "new value"; //editing value at fourth index........e.t.c
+	{						//change some values
+
+		var object_index_array = Object.keys(name_of_object);
+
+			name_of_object[object_index_array[0]] = new_value1; //editing value at first index
+			name_of_object[object_index_array[1]] = new_value2; //editing value at second index
+			name_of_object[object_index_array[2]] = new_value3; //editing value at third index
+			name_of_object[object_index_array[3]] = new_valueetc; //editing value at fourth index........e.t.c
+			return (Object.values(name_of_object));
 	}
+}
+}
 /* Edit code ends here */
 
 //Temporary deleting function
@@ -106,16 +110,16 @@ for(let i=0; i<my_address_book.length; i++) // address book is an array containi
 		{ 
 			var gona_delete_you = true;
 		} 
-	}
   if (gona_delete_you==true) 
   	{
   		my_trush_can.push(my_address_book[i]);
         my_address_book.splice(i,1);
-        return (my_address_book[i] + "has been deleted!!!!. It can be restored from the trush can");
+        return (name_of_object + "has been deleted!!!!. It can be restored from the trush can");
     }
   }
+}
 
-  //Recover function
+  //Recover function takes in the name of the object you want to recover as argument
   var restore = (name_of_object) => {
   var object_present = false;
   for(var i=0; i<my_trush_can.length ; i++)
@@ -123,10 +127,14 @@ for(let i=0; i<my_address_book.length; i++) // address book is an array containi
     if(my_trush_can[i] === name_of_object){
       object_present = true;
     }
-    if(object_present === true){
+   if(object_present === true){
       my_address_book.push(my_trush_can[i]);
       my_trush_can.splice(i,1);
-      return (my_trush_can[i] + "has been restored!!!!");
+      return (name_of_object + "has been restored!!!!");
   }
 }
 }
+
+// console.log(delete_some_object(scosh));
+// console.log(restore(scosh));
+console.log(deleting_you(scosh));
