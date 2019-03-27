f86600ae2f88ba4cf6a1f6e678f6934192b8b04a
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-27 04:16:22 +0300

    editing my code

--- objects_assignment.js
+++ objects_assignment.js
@@ -10,20 +10,26 @@ Store the Address as an object which contains street, city, country.
 Create a method that returns the full Address of the user in form of street + ", " + city + " " + state
 ____________________________________________________________________________________________________
 */
-function Contact(first_name, second_name, phone_number, , address)
+function Contact(first_name, second_name, phone_number, email, address)
 	{
 		this.first_name = first_name;
 		this.second_name = second_name;
 		this.phone_number = phone_number;
-		this.street = "";
-		this.city =
-		country
-		this.address = { street: this.street, city: '', country: '' };
+		this.email = email;
+		this.address = address;
 
-		//this.my_address = function() {return this.address[0] + " " + this.address[1] + " " + this.address[2];}
+		this.my_address = function(street, city, state) {
+			var value_in_address =  this.address;
+			// this.street = street;
+			// this.city = city;
+			// this.state = state;
+			street=value_in_address[0];
+			city = value_in_address[1];
+			state= value_in_address[2];
+			return (street + ", " + city + " " + state);
 	};
 
-function my_address(street, city, state){this.street=street; this.city=city; this.state=state;}
+//function my_address(street, city, state){this.street=street; this.city=city; this.state=state;}
 var scosh = new Contact("scovia", "Nnamiggo", "0703268406", ["wakiso", "kampala", "uganda"]);
 
-console.log(scosh.address.street);
+console.log(scosh.address);
